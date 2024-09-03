714c585ffbbc3cd541865e95dd07d4548e372027
Rohan <160586814+Rohaan851@users.noreply.github.com>
2024-09-03 11:23:17 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+// Calculator-Ass
+
+
+// let num1 = +prompt("Enter your first number :");
+// let opperator = prompt("Enter your Operator:");
+// let num2 = +prompt("Enter your second number :");
+
+// if(opperator === '+'){
+//     let sum = num1 + num2;
+//     document.write(`${num1} + ${num2} = ${sum}`);
+// }
+// if(opperator === '-'){
+//     let subtraction = num1 - num2;
+//     document.write(`${num1} + ${num2} = ${subtraction}`);
+// }
+// if(opperator === '*'){
+//     let multiplication = num1 * num2;
+//     document.write(`${num1} * ${num2} = ${multiplication}`);
+// }
+// if(opperator === '/'){
+//     let division = num1 / num2;
+//     document.write(`${num1} / ${num2} = ${division}`);
+// }
+// if(opperator === '%'){
+//     let percentage = num1 % num2;
+//     document.write(`${num1} % ${num2} = ${percentage}%`);
+// }
+
+// let arr = [1, 2, 1];
+// let result = arr.concat(arr);
+// console.log(result);
+
